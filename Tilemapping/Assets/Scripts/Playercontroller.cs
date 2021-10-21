b7bc08b65876524a06219347217dee035281b838
angelapage <angelapage1617@gmail.com>
2021-10-21 13:46:19 -0400

    Challenge commit

--- Tilemapping/Assets/Scripts/Playercontroller.cs
+++ Tilemapping/Assets/Scripts/Playercontroller.cs
@@ -5,34 +5,73 @@ using UnityEngine.UI;
 
 public class Playercontroller : MonoBehaviour
 {
-
     private Rigidbody2D rd2d;
-
     public float speed;
+    private bool facingRight = true;
+    private bool isJumping;
 
     public Text score;
+    public Text winText;
+    public Text livesText;
 
     private int scoreValue = 0;
+    private int lives;
+    
 
-    // Start is called before the first frame update
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    public AudioSource musicSource;
+    Animator anim;
+
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
         score.text = scoreValue.ToString();
+        lives = 3;
+
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
+
+        winText.text = "";
+        livesText.text = "";
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
-        
-        if (Input.GetKey("escape"))
+
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            Application.Quit();
+            anim.SetInteger("State", 1);
         }
 
+         if (Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+         if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+         if (Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (facingRight == false && hozMovement > 0)
+         {
+            Flip();
+         }
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        } 
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -42,18 +81,65 @@ public class Playercontroller : MonoBehaviour
             scoreValue += 1;
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+
+            if (scoreValue == 4)
+            {
+             transform.position = new Vector2(50.0f, 0.0f);
+             lives = 3;
+            }      
         }
 
+        if (scoreValue == 9)
+        {
+            scoreValue += 1;
+            musicSource.Stop();
+            winText.text = "You Win! Game made by Angela Page";
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+        }
+
+        else if (collision.collider.tag == "Enemy")
+        {
+            lives = lives - 1;
+            Destroy(collision.collider.gameObject);
+        }
+
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives <=0)
+        {
+            winText.text = "You lose!";
+        }
+
+        if (lives == 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.collider.tag == "Ground")
-        {
-            if (Input.GetKey(KeyCode.W))
+        {  
+            if (isJumping)
             {
-                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+                isJumping = false;
+                anim.SetBool("Jump", isJumping);
             }
+
+            if (Input.GetKeyDown(KeyCode.W))
+                {
+                   rd2d.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+                   isJumping = true;
+                   anim.SetBool("Jump", isJumping);
+                }     
         }
     }
+
+    void Flip()
+   {
+     facingRight = !facingRight;
+     Vector2 Scaler = transform.localScale;
+     Scaler.x = Scaler.x * -1;
+     transform.localScale = Scaler;
+   }
 }
